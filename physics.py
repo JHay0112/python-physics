36ab14c15a420ec5679fe6df484ee725c4c2023b
Jordan Hay <jordanhay0112@gmail.com>
2020-07-28 12:45:48 +1200

    Update physics objects

--- physics.py
+++ physics.py
@@ -1,9 +1,8 @@
 '''
 
-    physics.py
 
-    Author: Jordan Hay
-    Date: 2020-07-24
+Author: Jordan Hay
+Date: 2020-07-24
 
 '''
 
@@ -28,7 +27,7 @@ class Vector:
 
         # Set input to object vars after xy to polar conversion
         self.magnitude, self.argument = xy_to_polar(x, y)
-            
+
     # Generate vector from polar description
     def from_polar(self, magnitude, argument):
 
@@ -49,7 +48,6 @@ class Vector:
         return(x, y)
 
     # Add another vector object to our vector object
-    def add_vectors(self, add_vectors):
     def add(self, add_vectors):
 
         # Get the x and y values of original vector
@@ -57,154 +55,70 @@ class Vector:
 
         for add_vector in add_vectors:
 
-            x, y += add_vector.return_xy()
-        
+            x += add_vector.return_xy()[0]
+            y += add_vector.return_xy()[1]
+
         # Assign converted to polar values to own
         self.argument, self.magnitude = xy_to_polar(x, y)
 
-'''
-# Stores an object that has physics applied to it
 class PhysicsObject:
 
-    # Initialisaion
-    # name (str) - The name of the object, to make it easier to identify which object we are getting the name of
-    # environment (obj) - The PhysicsEnvironment that the physics object is apart of
-    # mass (int) - Mass in kgs of the objects
-    # velocity (obj) - Vector describing the initial velocity of the physics object
-    # position (dict) - Cartesian coordinates of the object
-    # forces (list) - List of vector forces applied to the object
-    # init_time (int) - Time in seconds at which the velocity vector applies to
-    def __init__(self, name, environment, mass, velocity, position, forces = [], init_time = None):
+    # Initialisation
+    def __init__(self, mass = 0, init_velocity = Vector(0, 0), init_position = [0, 0], acceleration_vectors = [], init_time = 0):
 
-        # Assign parameters to object attributes
-        self.name = name
-        self.environment = environment
-        self.mass = mass
-        self.init_velocity = velocity
-        self.velocity = velocity
-        self.position = position
-        self.start_position = copy.copy(position) # Using copy to ensure changes do not link
-        self.forces = forces
-        self.forces.extend(self.environment.forces) # Add physics environement forces to item
-        self.init_time = init_time # In a sense this is the absolute time
-        self.time = 0 # Making this time relative to the vector
+        global physics_objects
 
-        self.environment.objects.append(self) # Append this object to list of physics object in the environement
+        self.mass = mass # Mass of our object
+        self.init_velocity = init_velocity # Velocity of the object at initialisation
+        self.init_position = init_position
+        self.acceleration_vectors = acceleration_vectors
+        self.init_time = init_time
+        self.time = 0
 
-        # If init time has not been set make it that same as the current phys time
-        if(self.init_time is None):
-            self.init_time = self.environment.phys_time
+        physics_objects.extend(self) # Add self to list of physics_objects
 
-        self.set_time(self.init_time) # Set time to be init_time
+    # Update object adjusted time using global physics time and own init time
+    def update_time(self):
 
-    # Adjust time relative with the init time and then set as relative time
-    def set_time(self, time):
+        global physics_time # Get physics time global
 
-        self.time = time - self.init_time # Set time adjusted relative to init_time 
+        return(physics_time - self.init_time) # Return time adjusted with vector init_time
 
-    # Print out information relevant to self
-    def info(self):
+    # Calculate the acceleration acting on the object as a vector
+    def acceleration(self):
 
-        print(f"Name: {self.name}, Mass: {self.mass}kg, Velocity: {self.velocity.magnitude}m/s, momentum: {self.momentum().magnitude}kgm/s, X: {self.position[0]}m Y: {self.position[1]}m, X Velocity: {self.velocity.return_xy()[0].magnitude}m/s, Y Velocity: {self.velocity.return_xy()[1].magnitude}m/s")
+        return(Vector(0, 0).add(self.acceleration_vectors))
 
-    # Caculate and return object momentum vector
+    # Calculte the momentum of the object as a vector
     def momentum(self):
 
-        return(Vector(self.mass * self.direction_vector().magnitude, self.direction_vector().argument))
+        return(Vector(self.direction().magnitude * self.mass, self.direction().argument))
 
-    # Calculate the resultant vector
-    def resultant_vector(self):
+    # Calculate the current direction and magnitude of velocity at the time
+    def velocity(self):
 
-        result = self.init_velocity # Set vector to be velocity
+        update_time()
 
-        result.add(self.forces_vector()) # add forces vector to velocity vector
+        init_vel_x, init_vel_y = self.init_velocity.return_xy()
 
-        return(result)
+        accel_x, accel_y = acceleration().return_xy()
 
-    # Calculate the vector of applied forces
-    def forces_vector(self):
+        vel_x, vel_y = (velocity_equation(init_vel_x, accel_x, time), velocity_equation(init_vel_y, accel_y, self.time))
 
-        result = Vector(0, 0) # Create blank vector
+        magnitude, argument = xy_to_polar(vel_x, vel_y)
 
-        # For every force applied to object
-        for force in range(0, len(self.forces)):
+        return(Vector(magnitude, argument))
 
-            result.add(self.forces[force]) # Add force vector to result vector
+    def position(self):
 
-        return(result)
+        init_vel_x, init_vel_y = self.init_velocity.return_xy()
 
-    # Vector that returns vector at point in time
-    def direction_vector(self):
+        accel_x, accel_y = acceleration().return_xy()
 
-        # Get the initial velocity
-        init_vel_x = self.init_velocity.return_xy()[0].magnitude
-        init_vel_y = self.init_velocity.return_xy()[1].magnitude
+        x = distance_equation(init_vel_x, self.time, accel_x)
+        y = distance_equation(init_vel_y, self.time, accel_y)
 
-        # Get accelerations
-        accel_x = self.forces_vector().return_xy()[0].magnitude
-        accel_y = self.forces_vector().return_xy()[1].magnitude
-
-        # Calculate x and y vectors for current velocity
-        vel_x = velocity_equation(init_vel_x, accel_x, self.time)
-        vel_y = velocity_equation(init_vel_y, accel_y, self.time)
-
-        result = Vector(0, 0) # Blank vector
-        result.add(Vector(vel_x, 0)) # Add the x vector
-        result.add(Vector(vel_y, 90)) # Add the y vector
-
-        return(result)
-
-    # Update the vector information
-    def update(self):
-
-        # Get x and y velocity
-        vel_x, vel_y = self.init_velocity.return_xy()
-
-        # Set to be magnitudes
-        vel_x = vel_x.magnitude
-        vel_y = vel_y.magnitude 
-
-        # Get x and y acceleration magnitude
-        accel_x = self.forces_vector().return_xy()[0].magnitude
-        accel_y = self.forces_vector().return_xy()[1].magnitude
-        
-        # Set the current velocity to be the direction vector
-        self.velocity = self.direction_vector()
-
-        # Set the updated position
-        self.position[0] = distance_equation(vel_x, self.time, accel_x) + self.start_position[0]
-        self.position[1] = distance_equation(vel_y, self.time, accel_y) + self.start_position[1]
-
-    # Collide our object with another
-    def collide(self, collision_object):
-
-        print(f"Collision between {self.name} and {collision_object.name}")
-
-        # Add our total momentum together on each axis
-        x_total_momentum = self.momentum().return_xy()[0]
-        x_total_momentum.add(collision_object.momentum().return_xy()[0])
-        y_total_momentum = self.momentum().return_xy()[1]
-        y_total_momentum.add(collision_object.momentum().return_xy()[1])
-
-        # Total mass
-        total_mass = self.mass + collision_object.mass
-
-        # Divide the magnitudes by the mass
-        x_velocity = x_total_momentum.magnitude / total_mass
-        y_velocity = y_total_momentum.magnitude / total_mass
-
-        # Add the vectors together
-        result = Vector(0, 0)
-        result.add(Vector(x_velocity, 0))
-        result.add(Vector(y_velocity, 90))
-
-        # Set the new initial velocities and times
-        self.init_velocity = result
-        self.init_time = self.time
-
-        collision_object.init_velocity = result
-        collision_object.init_time = collision_object.time
-'''
+        return([x, y])
 
 # -- Functions --
 
@@ -244,7 +158,6 @@ def xy_to_polar(x, y):
 # -- Variables --
 
 physics_objects = [] # List of all physics objects
+physics_time = 0 # Record what "time" it is in the system 
 
 # -- Main --
-
-
