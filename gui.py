981db213fb69f7da6fa9cb8ceb53f2ba00f9d0be
Jordan Hay <jordanhay0112@gmail.com>
2020-08-11 19:56:59 +1200

    Some actual GUI elements

--- gui.py
+++ gui.py
@@ -11,11 +11,16 @@ Graphics for physics engine
 
 # -- Imports --
 
-import physics # The physics engine
+import physics as phy # The physics engine
 import tkinter as tk # GUI
 from tkinter import ttk # More GUI
 
 # -- Classes --
+
+# The environment that an object or objects exist in
+class GUIEnvironment:
+
+    pass
 
 # -- Functions --
 
@@ -23,8 +28,19 @@ from tkinter import ttk # More GUI
 
 # -- Variables --
 
+root = tk.Tk() # Root GUI instance
+
 # -- Main --
 
 if(__name__ == "__main__"):
 
-    pass
+    # -- tk setup --
+    root.title("Physics Simulation")
+    root.attributes("-fullscreen", True)
+
+    # Exit button
+    exit_btn = ttk.Button(root, text = "Close", command = root.destroy)
+    exit_btn.place(x = 10, y = 10)
+
+    # -- Mainloop --
+    root.mainloop()
