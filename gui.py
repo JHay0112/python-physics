40c538cb630bac538042e3f2698d09ca3753e732
Jordan Hay <jordanhay0112@gmail.com>
2020-08-11 12:37:52 +1200

    Initialise gui python file

---
+++ gui.py
@@ -0,0 +1,30 @@
+'''
+
+gui.py
+
+Author: Jordan Hay
+Date: 2020-08-11
+
+Graphics for physics engine
+
+'''
+
+# -- Imports --
+
+import physics # The physics engine
+import tkinter as tk # GUI
+from tkinter import ttk # More GUI
+
+# -- Classes --
+
+# -- Functions --
+
+# -- Constants --
+
+# -- Variables --
+
+# -- Main --
+
+if(__name__ == "__main__"):
+
+    pass
